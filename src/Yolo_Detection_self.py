b7e32e0c8648b08bf798f6481ac29ca97aa9ccc3
tonu1990 <tonujk@gmail.com>
2025-10-26 01:43:27 +0100

    Gstreamer-Pyside6-Yolo

--- src/Yolo_Detection_self.py
+++ src/Yolo_Detection_self.py
@@ -2,27 +2,32 @@ import gi
 gi.require_version('Gst' , '1.0')
 from gi.repository import Gst, GLib
 
-pipeline_str = ( "v4l2src device=/dev/video0 ! " 
-                "image/jpeg,width=640,height=480,framerate=30/1 !" 
-                "jpegdec !"
-                "xvimagesink" )
+def main():
 
-print("starting Gstreamer pipeline ")
+    pipeline_str = ( "v4l2src device=/dev/video0 ! " 
+                    "image/jpeg,width=640,height=480,framerate=30/1 !" 
+                    "jpegdec !"
+                    "xvimagesink" )
 
-# create pipeline from String
-pipeline = Gst.parse_launch(pipeline_str)
+    print("starting Gstreamer pipeline ")
 
-#start Playing
-pipeline.set_state(Gst.state.PLAYING)
-print("Pipeline started Playing")
+    # create pipeline from String
+    pipeline = Gst.parse_launch(pipeline_str)
 
-# create a Main loop for running Pipeline 
-loop=GLib.MainLoop()
+    #start Playing
+    pipeline.set_state(Gst.state.PLAYING)
+    print("Pipeline started Playing")
 
-try :
-    loop.run()
-except KeyboardInterrupt:
-        print("\n\nReceived interrupt signal. Stopping pipeline...")
+    # create a Main loop for running Pipeline 
+    loop=GLib.MainLoop()
 
-pipeline.set_state(Gst.state.NULL)
-print("Pipeline stopped..")
+    try :
+        loop.run()
+    except KeyboardInterrupt:
+            print("\n\nReceived interrupt signal. Stopping pipeline...")
+
+    pipeline.set_state(Gst.state.NULL)
+    print("Pipeline stopped..")
+
+if __name__ == "__main__" :
+     main()
